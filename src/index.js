734c54279f5fcea7103760c64f748a5896f65217
Dmitry Drevich <jeidan2012@gmail.com>
2023-02-24 15:24:32 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+	if (str.length % 2 > 0) return false; // if str length is odd no need to go next steps;
+
+	let result = str;
+	for (let i = 0; i < str.length; i++) {
+		for (let j = 0; j < bracketsConfig.length; j++) {
+			result = result.replace(bracketsConfig[j][0] + bracketsConfig[j][1], '');
+		}
+	}
+	return result.length === 0;
 }
